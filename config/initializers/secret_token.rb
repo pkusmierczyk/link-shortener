b85fa87864fad19efa202fec6863f4368166e177
Piotr Kusmierczyk <kusmierczyk.p@gmail.com>
2013-05-27 22:54:43 +0200

    projekt rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkShortener::Application.config.secret_token = '5507286cd98d0e5e8ef873778c328ef79171df312838be80905f44f0d5f35b4c4c46f69669f738ff598b61c66ca5346dae4aad804d4053685027ab401523d2b0'
